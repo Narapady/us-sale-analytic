da59e2aac39e0f5059d7c849ce0d0dbedae17a21
Narapady <chhuoynarapady@yahoo.com>
2023-11-02 23:47:05 -0700

    work on loading data to bigquery

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 
 def preprocess(file_path: str):
@@ -21,10 +25,44 @@ def preprocess(file_path: str):
 
     df.to_csv("sales_preprocessed.csv", index=False)
 
-def load_to_bigquery():
-    pass
+
+def authenticate_bigquery(cred_file: str):
+    # Create credentials with the service account file and initialize a BigQuery client
+    credentials = service_account.Credentials.from_service_account_file(cred_file)
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+    return client
+
+
+def create_table_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
+    ...
+
+
+def load_to_bigquery(
+    client: bigquery.Client, dataset_id: str, table_id: str, file_path: str
+):
+    # Set the dataset and table where you want to upload the CSV file
+
+    # Create a job configuration
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True,
+    )
+
+    # Load the CSV file to BigQuery
+    load_job = client.load_table_from_uri(
+        file_path, f"{dataset_id}.{table_id}", job_config=job_config
+    )
+
+    load_job.result()  # Wait for the job to complete
+
+    destination_table = client.get_table(f"{dataset_id}.{table_id}")
+    print(f"Loaded {destination_table.num_rows} rows.")
+
 
 def main():
+    dataset_id = "us_sale_dataset"
+    table_id = "sales_processed"
     preprocess("sales.csv")
 
 
