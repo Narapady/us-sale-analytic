63379cfbae5bed1fbe5b3af90939caf03c14231b
Narapady <chhuoynarapady@yahoo.com>
2023-11-05 21:25:39 -0800

    done loading to bigquery

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import pandas as pd
 from google.cloud import bigquery
-from google.oauth2 import service_account
 
 
 def preprocess(file_path: str):
@@ -26,44 +25,58 @@ def preprocess(file_path: str):
     df.to_csv("sales_preprocessed.csv", index=False)
 
 
-def authenticate_bigquery(cred_file: str):
-    # Create credentials with the service account file and initialize a BigQuery client
-    credentials = service_account.Credentials.from_service_account_file(cred_file)
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+def bigquery_client(cred_file: str):
+    client = bigquery.Client.from_service_account_json(cred_file)
     return client
 
 
 def create_table_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
-    ...
+    try:
+        table_ref = client.dataset(dataset_id).table(table_id)
+        table = bigquery.Table(table_ref)
+        return client.create_table(table)
+    except Exception as e:
+        print(f"An error occurred while creating the table: {e}")
+        return None
 
 
-def load_to_bigquery(
-    client: bigquery.Client, dataset_id: str, table_id: str, file_path: str
-):
+def load_to_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
     # Set the dataset and table where you want to upload the CSV file
+    df = pd.read_csv("sales_preprocessed.csv")
 
+    schema = [
+        bigquery.SchemaField(col, "integer")
+        if df[col].dtype == "int64"
+        else bigquery.SchemaField(col, "float")
+        if df[col].dtype == "float64"
+        else bigquery.SchemaField(col, "string")
+        if df[col].dtype == "object"
+        else bigquery.SchemaField(col, "timestamp")
+        if df[col].dtype == "datetime64"
+        else None
+        for col in df.columns
+    ]
     # Create a job configuration
     job_config = bigquery.LoadJobConfig(
+        schema=schema,
         source_format=bigquery.SourceFormat.CSV,
         skip_leading_rows=1,
-        autodetect=True,
     )
 
-    # Load the CSV file to BigQuery
-    load_job = client.load_table_from_uri(
-        file_path, f"{dataset_id}.{table_id}", job_config=job_config
+    load_job = client.load_table_from_dataframe(
+        df, f"{dataset_id}.{table_id}", job_config=job_config
     )
-
-    load_job.result()  # Wait for the job to complete
-
-    destination_table = client.get_table(f"{dataset_id}.{table_id}")
-    print(f"Loaded {destination_table.num_rows} rows.")
+    load_job.result()
 
 
 def main():
+    service_account_key_path = Path.cwd() / "us-sale-cred.json"
+    client = bigquery_client(service_account_key_path)
     dataset_id = "us_sale_dataset"
     table_id = "sales_processed"
     preprocess("sales.csv")
+    if create_table_bigquery(client, dataset_id, table_id) is not None:
+        load_to_bigquery(client, dataset_id, table_id)
 
 
 if __name__ == "__main__":
