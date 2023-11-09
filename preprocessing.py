08a7a4d256d11d1954defc5de56a314e2b59d99f
Narapady <chhuoynarapady@yahoo.com>
2023-11-08 22:18:28 -0800

    add docstrings

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,17 @@ from google.cloud import bigquery
 
 
 def rename_columns(df: pd.DataFrame):
+    """
+    This function renames the columns of a DataFrame.
+    It splits the column names by space and joins them with an underscore.
+    If the column name is a single word, it is converted to lowercase.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame whose columns are to be renamed.
+
+    Returns:
+    list[str]: The list of renamed column names.
+    """
     cols: list[str] = []
     for col in df.columns:
         split_col = col.split(" ")
@@ -18,8 +29,9 @@ def rename_columns(df: pd.DataFrame):
 def preprocess(file_path: str):
     """
     This function preprocesses the data from a given file path.
-    It reads the data, modifies the column names, and changes the data types of certain columns.
-    The preprocessed data is then saved to a new CSV file.
+    It reads the data, modifies the column names, and changes
+    the data types of certain columns. The preprocessed data is
+    then saved to a new CSV file.
 
     Parameters:
     file_path (str): The path to the file to be preprocessed.
@@ -41,11 +53,32 @@ def preprocess(file_path: str):
 
 
 def bigquery_client(cred_file: str):
+    """
+    This function creates a BigQuery client from a service account JSON file.
+
+    Parameters:
+    cred_file (str): The path to the service account JSON file.
+
+    Returns:
+    bigquery.Client: The BigQuery client.
+    """
     client = bigquery.Client.from_service_account_json(cred_file)
     return client
 
 
 def create_table_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
+    """
+    This function creates a new table in BigQuery.
+
+    Parameters:
+    client (bigquery.Client): The BigQuery client.
+    dataset_id (str): The ID of the dataset where the table will be created.
+    table_id (str): The ID of the table to be created.
+
+    Returns:
+    bigquery.Table: The created table.
+    None: If an error occurred while creating the table.
+    """
     try:
         table_ref = client.dataset(dataset_id).table(table_id)
         table = bigquery.Table(table_ref)
@@ -56,6 +89,17 @@ def create_table_bigquery(client: bigquery.Client, dataset_id: str, table_id: st
 
 
 def load_to_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
+    """
+    This function loads a preprocessed CSV file to a BigQuery table.
+
+    Parameters:
+    client (bigquery.Client): The BigQuery client.
+    dataset_id (str): The ID of the dataset where the table will be created.
+    table_id (str): The ID of the table to be created.
+
+    Returns:
+    None
+    """
     # Set the dataset and table where you want to upload the CSV file
     df = pd.read_csv("sales_preprocessed.csv")
 
@@ -86,12 +130,16 @@ def load_to_bigquery(client: bigquery.Client, dataset_id: str, table_id: str):
 
 
 def main():
+    # Define the path to the service account key
     service_account_key_path = Path.cwd() / "us-sale-cred.json"
+    # Create a BigQuery client using the service account key
     client = bigquery_client(service_account_key_path)
-    dataset_id = "us_sale_dataset"
-    table_id = "sales_processed"
-    preprocess("sales.csv")
+    dataset_id = "us_sale_dataset"  # Define the dataset ID
+    table_id = "sales_processed"  # Define the table ID
+    preprocess("sales.csv")  # Preprocess the sales.csv file
+    # If the table is successfully created in BigQuery
     if create_table_bigquery(client, dataset_id, table_id) is not None:
+        # Load the preprocessed data to the BigQuery table
         load_to_bigquery(client, dataset_id, table_id)
 
 
